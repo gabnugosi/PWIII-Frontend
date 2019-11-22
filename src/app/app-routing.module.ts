5b2de0a609057a807c641de25b5226e2279d9b49
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2019-11-22 20:46:34 -0300

    Commit no projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaAutorComponent } from './lista-autor/lista-autor.component';
+import { NovoAutorComponent } from './novo-autor/novo-autor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listagem', component: ListaAutorComponent},
+  {path: 'novo', component: NovoAutorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaAutorComponent } from './lista-autor/lista-autor.component';
+import { NovoAutorComponent } from './novo-autor/novo-autor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaAutorComponent,
+    NovoAutorComponent
   ],
   imports: [
     BrowserModule,
