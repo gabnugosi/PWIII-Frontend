c021b3afd9dc371b0af095a1154219c3e07c10c4
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2019-11-22 21:54:26 -0300

    Exercicio 26

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaAutorComponent } from './lista-autor/lista-autor.component';
 import { NovoAutorComponent } from './novo-autor/novo-autor.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NovoAutorComponent } from './novo-autor/novo-autor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/autor.ts
+++ src/app/autor.ts
@@ -1,2 +1,4 @@
 export class Autor {
+    public nome : string;
 }
+
